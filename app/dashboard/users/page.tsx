b481668e4dd849587787ce76d12b3a50e0a05e38
Chandradeep <chandradeep@Chandradeeps-Mac-mini.local>
2026-02-23 23:19:02 +0530

    ts error

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -2,6 +2,8 @@
 import { getGuestUsers } from "@/lib/server/guestService";
 import UsersClient from "./UserClient";
 
+export const dynamic = "force-dynamic";
+
 export default async function UsersPage() {
   const guests = await getGuestUsers();
 
