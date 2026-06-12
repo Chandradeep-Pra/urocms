4dd57cbca8312cc155cd7f96dcb75a5992c5384b
Chandradeep Prasad <chandradeepprasad1998@gmail.com>
2026-06-13 00:12:08 +0530

    accept reroute from testing-zone

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,18 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   serverExternalPackages: ["firebase-admin"],
+  async rewrites() {
+    return [
+      {
+        source: "/web",
+        destination: "https://testing-zone-five.vercel.app/web",
+      },
+      {
+        source: "/web/:path*",
+        destination: "https://testing-zone-five.vercel.app/web/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
